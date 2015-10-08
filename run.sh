dcb59776669b75310f20f82911a3b1f12cf00dcc
Sho Mizoe <sho.mizoe@gmail.com>
2015-10-08 14:21:34 -0700

    added functionality to pass options

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 IMG_NAME="${1:-hadoop_test_env}"
 VM_NAME="${2:-dev}"
+DOCKER_OPTS="${3}"
 eval "$(docker-machine env ${VM_NAME})"
 docker build -t ${IMG_NAME} .
-docker run -it --rm  ${IMG_NAME}
+docker run ${DOCKER_OPTS} -it --rm  ${IMG_NAME}
